b43fe451d02fea477f96488dc3f526bf595df5f9
Steven Ang <spikegreen2@gmail.com>
2017-12-06 21:41:30 +0800

    Solution: Spinal Tap Caps

---
+++ 12-Spinal-Tap-Case.js
@@ -0,0 +1,8 @@
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+  return str.toLowerCase().replace(/\s|_/g, "-");
+}
+
+spinalCase('AllThe-small Things');
