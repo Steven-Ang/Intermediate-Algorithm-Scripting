3fa47fdbaa53d597f9ee0d99269be1db02dc2e7c
Steven Ang <spikegreen2@gmail.com>
2017-12-06 20:49:51 +0800

    Solution: Convert HTML Entities

---
+++ 11-Convert-HTML-Entities.js
@@ -0,0 +1,26 @@
+function convertHTML(str) {
+  // &colon;&rpar;
+  str = str.split("");
+  for (var i = 0; i < str.length; i++) {
+    switch(str[i]) {
+      case "&":
+        str[i] = "&amp;";
+        break;
+      case "<":
+        str[i] ="&lt;";
+        break;
+      case ">":
+        str[i] = "&gt;";
+        break;
+      case '"':
+        str[i] = '&quot;';
+        break;
+      case "'":
+        str[i]= "&apos;";
+        break;
+    }
+  }
+  return str.join("");
+}
+
+convertHTML('Stuff in "quotation marks"');
