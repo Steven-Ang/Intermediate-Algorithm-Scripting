8320220138b64b8e2e4ced4a2c0b3bbbdf426404
Steven Ang <spikegreen2@gmail.com>
2018-11-08 20:38:42 +0800

    Update 09-Boo-Who.js

--- 09-Boo-Who.js
+++ 09-Boo-Who.js
@@ -1,6 +1,6 @@
 function booWho(bool) {
   // What is the new fad diet for ghost developers? The Boolean.
-  return (bool === true || bool === false) ? true : false;
+  return (bool === true || bool === false);
 }
 
 booWho(null);
