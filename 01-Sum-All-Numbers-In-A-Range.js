b1fb53412b5b69bb4df671375f252ef74b307ae4
Steven Ang <spikegreen2@gmail.com>
2018-11-09 03:16:04 +0800

    Update 01-Sum-All-Numbers-In-A-Range.js

--- 01-Sum-All-Numbers-In-A-Range.js
+++ 01-Sum-All-Numbers-In-A-Range.js
@@ -1,3 +1,4 @@
+// Solution 1
 function sumAll(arr) {
   var results = [];
   var max = Math.max(arr[0], arr[1]);
@@ -10,6 +11,17 @@ function sumAll(arr) {
   return results.reduce(function(a, b) {
     return a + b;
   });
+}
+
+// Solution 2
+function sumAll(arr) {
+  // Sort the array by ASC order
+	arr.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = arr[0]; i <= arr[1]; i++) {
+		sum += i;
+  }
+	return sum;
 }
 
 sumAll([1, 4]);
