7039efd77ff470b56cf87da1cdebebf1a10d4e28
Steven Ang <spikegreen2@gmail.com>
2018-11-09 03:30:28 +0800

    Update 02-Diff-Two-Arrays.js

--- 02-Diff-Two-Arrays.js
+++ 02-Diff-Two-Arrays.js
@@ -1,11 +1,10 @@
 function diffArray(arr1, arr2) {
-  var results = arr1.concat(arr2);
-
-  return results.filter(function(val) {
-    if (arr1.indexOf(val) === -1 || arr2.indexOf(val) === -1) {
-      return val;
-    }
-  });
+  // Concat the two arrays and then filter the values later on
+	const results = arr1.concat(arr2);
+	return results.filter(val => {
+    // If there are unique value in any of the arrays, return them
+		if (arr1.indexOf(val) === -1 || arr2.indexOf(val) === -1) return val
+	});
 }
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
