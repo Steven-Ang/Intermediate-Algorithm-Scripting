328afc55c2e5660585ee17ffb971c85fee440c59
Steven Ang <spikegreen2@gmail.com>
2018-11-19 20:39:15 +0800

    Update 05-Search-And-Replace.js

--- 05-Search-And-Replace.js
+++ 05-Search-And-Replace.js
@@ -1,9 +1,9 @@
 function myReplace(str, before, after) {
-
+  // If the first character is uppercase, then the replacement should be uppercase as well
   if (before.charAt(0) === before.charAt(0).toUpperCase()) {
     after = after.charAt(0).toUpperCase() + after.slice(1);
   }
-
+  // Return the string
   return str.replace(before, after);
 }
 
