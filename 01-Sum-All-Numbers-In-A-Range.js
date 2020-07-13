e1164103ec062981d6e6ba084639ace4584d56e1
Steven Ang <spikegreen2@gmail.com>
2020-07-14 01:07:09 +0800

    Update 01-Sum-All-Numbers-In-A-Range.js

--- 01-Sum-All-Numbers-In-A-Range.js
+++ 01-Sum-All-Numbers-In-A-Range.js
@@ -24,4 +24,15 @@ function sumAll(arr) {
   return total;
 }
 
+// Solution 3
+function sumAll(arr) {
+  let lowest = arr[0] > arr[1] ? arr[1] : arr[0];
+  let highest = arr[0] > arr[1] ? arr[0] : arr[1];
+  let total = 0;
+  for (let i = lowest; i <= highest; i++) {
+    total += i;
+  }
+  return total;
+}
+
 sumAll([1, 4]);
