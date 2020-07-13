e33e148f9432db663abeff02ff7cbe5600d682fb
Steven Ang <spikegreen2@gmail.com>
2020-07-14 01:20:29 +0800

    Update 02-Diff-Two-Arrays.js

--- 02-Diff-Two-Arrays.js
+++ 02-Diff-Two-Arrays.js
@@ -7,4 +7,26 @@ function diffArray(arr1, arr2) {
 	});
 }
 
+// Solution #2
+function diffArray(arr1, arr2) {
+  if (arr1.length === 0) return arr2
+  else if (arr2.length === 0) return arr1;
+  else if (arr1.length === 0 && arr2.length === 0) return [];
+
+  const result = [];
+
+  function compare(arr1, arr2, arrToPush) {
+    arr1.forEach(item => {
+      if (!arr2.includes(item)) {
+        arrToPush.push(item);
+      }
+    });
+  }
+
+  compare(arr1, arr2, result);
+  compare(arr2, arr1, result);
+
+  return result;
+}
+
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
