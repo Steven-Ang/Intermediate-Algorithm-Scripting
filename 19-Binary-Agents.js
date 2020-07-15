3d560530723cb3f7b02e37dab10d8c151deff28a
Steven Ang <spikegreen2@gmail.com>
2020-07-16 00:27:33 +0800

    Update 19-Binary-Agents.js

--- 19-Binary-Agents.js
+++ 19-Binary-Agents.js
@@ -7,4 +7,12 @@ function binaryAgent(str) {
   return str.join("");
 }
 
+// Solution #2
+function binaryAgent(str) {
+  return str.split(' ').reduce((str, char) => {
+    let parseChar = parseInt(char, 2);
+    return str += String.fromCharCode(parseChar);
+  }, '');
+}
+
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
