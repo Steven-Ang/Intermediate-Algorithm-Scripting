d49bddfc7bb7ad53228d835017635a097d744e93
Steven Ang <spikegreen2@gmail.com>
2020-07-16 00:41:42 +0800

    Update 20-Everything-Be-True.js

--- 20-Everything-Be-True.js
+++ 20-Everything-Be-True.js
@@ -5,4 +5,12 @@ function truthCheck(collection, pre) {
   });
 }
 
+// Solution #2
+function truthCheck(collection, pre) {
+  for (let obj of collection) {
+    if (!obj[pre]) return false;
+  }
+  return true;
+}
+
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
