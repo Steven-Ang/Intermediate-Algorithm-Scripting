d5137b6169ec5eaaec51e99b7abda47c01bba0c9
Steven Ang <spikegreen2@gmail.com>
2020-07-15 22:15:32 +0800

    Update 17-Drop-It.js

--- 17-Drop-It.js
+++ 17-Drop-It.js
@@ -4,4 +4,22 @@ function dropElements(arr, func) {
   return arr;
 }
 
+// Solution #2
+function dropElements(arr, func) {
+  const result = [];
+  let ignoreFlag = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    let ele = arr[i];
+    if (func(ele)) {
+      result.push(ele);
+      ignoreFlag = true
+    } else if (ignoreFlag) {
+      result.push(ele);
+    }
+  }
+
+  return result;
+}
+
 dropElements([1, 2, 3], function(n) {return n < 3; });
